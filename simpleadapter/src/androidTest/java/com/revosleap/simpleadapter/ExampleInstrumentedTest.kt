24e281b00e012a10d819db951f66a138f39a58f1
Kevin-Kip <masterfork5@gmail.com>
2019-01-01 00:18:51 +0300

    Added gradle bintray libs

--- simpleadapter/src/androidTest/java/com/revosleap/simpleadapter/ExampleInstrumentedTest.kt
+++ simpleadapter/src/androidTest/java/com/revosleap/simpleadapter/ExampleInstrumentedTest.kt
@@ -1,13 +1,10 @@
 package com.revosleap.simpleadapter
 
-import android.content.Context
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
