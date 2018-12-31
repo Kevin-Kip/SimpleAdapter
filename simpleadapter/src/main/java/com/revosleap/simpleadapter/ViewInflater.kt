cdd2c612d653edee6bdc712f5eb09606868d00e2
Kevin-Kip <masterfork5@gmail.com>
2018-12-31 20:04:22 +0300

    Added code to inflate layout

--- simpleadapter/src/main/java/com/revosleap/simpleadapter/ViewInflater.kt
+++ simpleadapter/src/main/java/com/revosleap/simpleadapter/ViewInflater.kt
@@ -1,2 +1,10 @@
 package com.revosleap.simpleadapter
 
+import android.support.annotation.LayoutRes
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+fun ViewGroup.inflate(@LayoutRes layout: Int, attachToRoot: Boolean = false): View{
+    return LayoutInflater.from(context).inflate(layout,this, attachToRoot)
+}
