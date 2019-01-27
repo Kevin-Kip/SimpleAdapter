8ca7b103e5f7fbaebe66fbd025904e2a40ffef7a
Kevin-Kip <masterfork5@gmail.com>
2019-01-27 09:46:40 +0300

    Added longClick callback

--- simpleadapter/src/main/java/com/revosleap/simpleadapter/SimpleCallbacks.kt
+++ simpleadapter/src/main/java/com/revosleap/simpleadapter/SimpleCallbacks.kt
@@ -3,7 +3,7 @@ package com.revosleap.simpleadapter
 import android.view.View
 
 interface SimpleCallbacks{
-    fun onViewClicked(view: View, item: Any, position: Int)
     fun bindView(view: View, item: Any, position: Int)
+    fun onViewClicked(view: View, item: Any, position: Int)
     fun onViewLongClicked(it: View?, item: Any, position: Int)
 }
